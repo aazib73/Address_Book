5b72b42ceb2a36671d5a419e4f80ba0ca908bb79
Diego Urena <duren@cpp.edu>
2026-03-10 14:31:45 -0700

    Setting stage for Unity Unit tests

---
+++ test_address_book.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "unity.h"
+#include "address_book.h"
+#include "address_book_fops.h"
+#include "address_book_menu.h"
+
+void setUp(void) {
+
+}
+
+void tearDown(void) {
+    
+}
+
+//void test_blahblahblah(void) ...
+
+int main(void) {
+    //run the tests
+
+    //RUN_TEST(test_signature)
+}
